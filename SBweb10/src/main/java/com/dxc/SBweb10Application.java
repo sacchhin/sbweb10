30a93d60196a83fac4ecdcc57ac3206b937c2352
sachinsoppin <sachinsoppin@LAPTOP-UPG3F4KF>
2021-11-24 10:27:25 +0530

    boot first commit

---
+++ SBweb10/src/main/java/com/dxc/SBweb10Application.java
@@ -0,0 +1,13 @@
+package com.dxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBweb10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBweb10Application.class, args);
+	}
+
+}
---
+++ SBweb10/src/main/java/com/dxc/model/Student.java
@@ -0,0 +1,19 @@
+package com.dxc.model;
+
+public class Student {
+	public int getRoll() {
+		return roll;
+	}
+	public void setRoll(int roll) {
+		this.roll = roll;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	private int roll;
+	private String sname;
+
+}
